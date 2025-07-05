9a35f1bf8b2029aecd99f23e867a3316a6f9a7c2
kriminilla <hafidhabbypratama@gmail.com>
2025-07-05 13:47:01 +0800

    coba pull

---
+++ database/migrations/2025_06_11_160110_kepala_prodi.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kepala_prodi', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->primary();
+            $table->char('nip', 18);
+            $table->char('nidn', 10);
+            $table->string('angkatan', 5);
+            $table->string('telepon', 20);
+            $table->unsignedInteger('kode_prodi');
+            $table->tinyInteger('is_active');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('kode_prodi')->references('kode_prodi')->on('prodi')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kepala_prodi');
+    }
+};
---
+++ database/migrations/2025_06_11_163933_skpi.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('skpi', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('mahasiswa_id');
+            $table->unsignedInteger('kepala_prodi_id');
+            $table->string('nomor', 45);
+            $table->string('link', 200);
+            $table->string('status', 45);
+            $table->timestamps();
+
+            $table->foreign('mahasiswa_id')->references('user_id')->on('mahasiswa')->onDelete('cascade');
+            $table->foreign('kepala_prodi_id')->references('user_id')->on('kepala_prodi')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('skpi');
+    }
+};
